9bf0e0a87dfebfcc8f227a343a4757649088fecc
unknown <user@a5comp1.step.itstep.rv.ua>
2015-05-26 22:01:08 +0300

    add JSON FTP

---
+++ itstep/classwork/JSONserialize/JSONserialize/DubUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONserialize
+{
+    class DubUser
+    {
+        public string _name { get; set; }
+        public string _password { get; set; }
+        public int _age { get; set; }
+        public List<string> _subjects { get; set; }
+    }
+}
---
+++ itstep/classwork/JSONserialize/JSONserialize/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JSONserialize
+{
+    class User
+    {
+        public string _name;
+        public string _password;
+        public int _age;
+        [JsonProperty ("subsub")]
+        public string[] _subjects;
+        public string _some;
+
+        public User(string name, string password, int age)
+        {
+            this._name = name;
+            this._password = password;
+            this._age = age;
+            this._subjects = new[]
+            {
+                "sub1",
+                "sub1",
+                "sub1",
+                "sub1"
+            };
+            this._some = "test";
+        }
+    }
+}
