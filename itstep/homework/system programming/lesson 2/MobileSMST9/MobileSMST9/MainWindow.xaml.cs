af6a6ac5342848613b97e940f539e58da7ae00e4
pasha <zhmakp@gmail.com>
2015-06-01 03:24:58 +0300

    Add some function

--- itstep/homework/system programming/lesson 2/MobileSMST9/MobileSMST9/MainWindow.xaml.cs
+++ itstep/homework/system programming/lesson 2/MobileSMST9/MobileSMST9/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,63 @@ namespace MobileSMST9
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool isNewBtn;
+        private Button curBtn;
+        private int index;
+
+        private List<string> dict; 
         public MainWindow()
         {
             InitializeComponent();
+
+            dict = new List<string>();
+        }
+
+        private void btnClear_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click_1(object sender, RoutedEventArgs e)
+        {
+            dict.Add(txtField.SelectedText);
+        }
+
+        private void btnNum_Click_1(object sender, RoutedEventArgs e)
+        {
+            if(curBtn == null || curBtn != sender as Button)
+            {
+                index = 0;
+                isNewBtn = true;
+                curBtn = sender as Button;
+            }
+            
+            char[] symbols = Regex.Replace(curBtn.Content.ToString(), @"\s+", "").ToCharArray();
+
+            if(index == symbols.Length)
+            {
+                index = 0;
+            }
+            if (FindEnterWordInDict(txtField.Text.Split(' ').Last()) != "")
+            {
+                txtField.AppendText(FindEnterWordInDict(txtField.Text.Split(' ').Last()));
+                return;
+            }
+            txtField.AppendText(symbols[index].ToString());
+
+            index++;
+        }
+
+        private  string FindEnterWordInDict(string inWord)
+        {
+            foreach (var word in dict)
+            {
+                if(word.Contains(inWord))
+                {
+                    return word;
+                }
+            }
+            return "";
         }
     }
 }
