be038b13a5316772a3c15ab1cd25864acbfbdf81
pasha <zhmakp@gmail.com>
2015-06-17 16:55:00 +0300

    change

---
+++ itstep/classwork/ProtoBuf/JSONserialize/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using ProtoBuf;
+namespace JSONserialize
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private User user;
+        private string json_temp;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSerialize_OnClick(object sender, RoutedEventArgs e)
+        {
+
+            TestProtoBuf(user, "Test");
+        }
+
+        private void BtnSave_OnClick(object sender, RoutedEventArgs e)
+        {
+            user = new User(txtName.Text, txtPassword.Text, 20);
+        }
+
+        private void BtnDeserialize_OnClick(object sender, RoutedEventArgs e)
+        {
+           
+            //DubUser user = Serializer.Deserialize<DubUser>(json_temp);
+        }
+
+        private static void TestProtoBuf(User user, string fileName)
+        {
+            var stopwatch = new Stopwatch();
+            using (var file = File.Create(fileName))
+            {
+                stopwatch.Start();
+
+     
+                    file.Position = 0;
+                    Serializer.Serialize(file, user);
+                    file.Position = 0;
+                    var restoredTasks = Serializer.Deserialize<User>(file);
+
+
+                stopwatch.Stop();
+
+                Debug.WriteLine(" iteration in {0} ms", stopwatch.ElapsedMilliseconds);
+            }
+        }
+    }
+}
---
+++ itstep/classwork/ProtoBuf/JSONserialize/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ProtoBuf;
+
+namespace JSONserialize
+{
+    [ProtoContract]
+    class User
+    {
+        [ProtoMember(1)]
+        public string _name;
+        [ProtoMember(2)]
+        public string _password;
+        [ProtoMember(3)]
+        public int _age;
+        [ProtoMember(4)]
+        public string[] _subjects;
+        [ProtoMember(5)]
+        public string _some;
+        public  User()
+        {
+            
+        }
+        public User(string name, string password, int age)
+        {
+            this._name = name;
+            this._password = password;
+            this._age = age;
+            this._subjects = new[]
+            {
+                "sub1",
+                "sub1",
+                "sub1",
+                "sub1"
+            };
+            this._some = "test";
+        }
+    }
+}
